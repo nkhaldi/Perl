83e155daa331a079158877b71ec30db0de284c39
Narek <mnk80898@gmail.com>
2019-10-08 03:59:03 +0300

    comparser

--- comment_parser.pl
+++ comment_parser.pl
@@ -2,25 +2,38 @@
 
 use 5.016;
 
-@ARGV = ();
 $^I = ".copy";
 
-system('find . -type f -name "*.c" > files');
-system('find . -type f -name "*.h" >> files');
+my $path = "";
+if (@ARGV) {
+	for (@ARGV) {
+		$path .= "$_ ";
+	}
+} else {
+	$path = ".";
+}
 
+system("find $path -type f -name '*.c' > files");
+system("find $path -type f -name '*.h' >> files");
+
+my @files;
 my $fd = open(FILES, '<', "files") or die "Can't open file";
 while (<FILES>) {
 	s/\n$//g;
-	push(@ARGV, $_);
+	push(@files, $_);
 }
-
 close($fd);
 system("rm files");
-die "No such files" unless @ARGV;
+
+die "No such files" unless @files;
+@ARGV = @files;
 
 while (<>) {
 	s/\s*\/\/.*//g;
 	print;
 }
 
+for (@files) {
+	system ("rm $_.copy");
+}
 say "***PARSED***";
