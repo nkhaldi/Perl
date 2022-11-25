eed7376a150a7355ef9773e3ac6865faed27fea1
nkhaldi <mnk80898@gmail.com>
2022-11-25 11:36:09 +0400

    made simle curle request script

--- curl_request.pl
+++ curl_request.pl
@@ -7,8 +7,21 @@ use Data::Dumper;
 use WWW::Curl::Easy;
 
 use constant {
-    URL => "https://www.cbr-xml-daily.ru/daily_json.js"
+    VER => 0,
+    URL => "https://www.cbr-xml-daily.ru/daily_json.js",
 };
 
 
-my $curl = WWW::Curl::Easy->new();
+my $curl = WWW::Curl::Easy->new;
+$curl->setopt(CURLOPT_URL, URL);
+$curl->setopt(CURLOPT_VERBOSE, VERB);
+$curl->setopt(CURLOPT_SSL_VERIFYHOST, 0);
+$curl->setopt(CURLOPT_SSL_VERIFYPEER, 0);
+
+my $response;
+$curl->setopt(CURLOPT_WRITEDATA, \$response);
+my $retcode = $curl->perform;
+my $respcode = $curl->getinfo(CURLINFO_HTTP_CODE);
+
+say "$retcode - $respcode";
+say $response;
